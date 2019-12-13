0c8fd9c203189ea33afdc0dd141b444b92b79f36
Drew Letcher <drew@drewletcher.net>
2019-12-12 23:03:40 -0600

    add notify api

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ exports.User = require('./lib/user')
 
 exports.Account = require("./lib/account")
 exports.Settings = require("./lib/settings")
+exports.Notify = require("./lib/notify")
 exports.Storage = require("./lib/storage")
 exports.Import = require("./lib/import")
 exports.Docs = require("./lib/docs")
