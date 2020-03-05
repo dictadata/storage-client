a834c129a170cd52b1d958549c236e9687a4e2c6
Drew Letcher <drew@drewletcher.net>
2020-03-05 08:35:42 -0600

    req.body.data and StorageResults

--- test/main.js
+++ test/main.js
@@ -8,27 +8,49 @@ console.log("dicta.io-client tests");
 console.log(process.env.NODE_ENV);
 console.log(process.env.VUE_APP_DICTA_API);
 
-let user = new client.User('drewlab', 'pi7za1')
-
 /**
- * login
+ * Account
  */
 async function accountTests() {
-  console.log("=== register test");
+  console.log("===== Accounts =====")
+
+  let user = new client.User('tester', 'testy')
+
+  console.log("=== account register test");
   try {
     let account = new client.Account(user)
-    let result = await account.register()
-    console.log("result: " + JSON.stringify(result, null, "  "))
+    let results = await account.register()
+    console.log("results: " + JSON.stringify(results, null, "  "))
   }
   catch (err) {
     console.log(err);
   }
 
-  console.log("=== login test");
+  console.log("=== account login test");
   try {
     let account = new client.Account(user)
-    let result = await account.login()
-    console.log("result: " + JSON.stringify(result, null, "  "))
+    let results = await account.login()
+    console.log("results: " + JSON.stringify(results, null, "  "))
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  console.log("=== account store test");
+  try {
+    let account = new client.Account(user)
+    let results = await account.store()
+    console.log("results: " + JSON.stringify(results, null, "  "))
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  console.log("=== account dull test");
+  try {
+    let account = new client.Account(user)
+    let results = await account.dull()
+    console.log("results: " + JSON.stringify(results, null, "  "))
   }
   catch (err) {
     console.log(err);
@@ -36,8 +58,64 @@ async function accountTests() {
 }
 
 /**
- * 
+ * Docs
  */
+async function docsTests() {
+  console.log("===== Docs =====")
 
-// test runners
-accountTests();
+  let user = new client.User('editor', 'editor')
+  let account = new client.Account(user)
+  let result = await account.login()
+  console.log("login: " + result)
+
+  console.log("=== docs store test");
+  try {
+    let doc = client.Docs.newDoc()
+    doc["docid"] = 'my new doc'
+    doc["title"] = 'my new doc'
+    doc['byline'] = 'drew'
+    doc["article"] = 'some text in my new doc'
+
+    let docs = new client.Docs(user)
+    let results = await docs.storeDoc(doc)
+    console.log("results: " + JSON.stringify(results, null, "  "))
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  console.log("=== docs recall test");
+  try {
+    let docs = new client.Docs(user)
+    let results = await docs.recallDoc("my new doc")
+    console.log("results: " + JSON.stringify(results, null, "  "))
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+  console.log("=== docs retrieve test");
+  try {
+    let pattern = {
+      match: {
+        byline: 'drew'
+      }
+    }
+    let docs = new client.Docs(user)
+    let results = await docs.retrieveDocs(pattern)
+    console.log("results: " + JSON.stringify(results, null, "  "))
+  }
+  catch (err) {
+    console.log(err);
+  }
+
+}
+
+
+async function main() {
+  // test runners
+  //await accountTests();
+  await docsTests();
+}
+
+main();
