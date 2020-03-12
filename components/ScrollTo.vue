7e8df37c3f42597de8f82c8c9525a16b0c52f521
Drew Letcher <drew@drewletcher.net>
2020-03-12 17:14:05 -0500

    adding all components to index

---
+++ components/ScrollTo.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="scroller-type">
+    <div class="container">
+      <p>
+      <a class="scroller-btn" :href="target">
+        <img alt="Scroll Down" :src="image" />
+      </a>
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'scroll-to',
+  props: {
+    target: {
+      type: String,
+      default: '#'
+    },
+    image: {
+      type: String,
+      default: "/img/arrow-down.png"
+    }
+  },
+  mounted() {
+    console.log(this.image)
+    // From testing, without a brief timeout, it won't work.
+    if (this.$route.hash)
+      setTimeout(() => this.scrollTo(this.$route.hash), TIMEOUT)
+  },
+  methods: {
+    scrollTo: function(hashtag) {
+      setTimeout(() => {
+        location.href = hashtag
+      }, TIMEOUT)
+    }
+  }
+}
+</script>
+<style></style>
--- components/index.js
+++ components/index.js
@@ -1,13 +1,38 @@
+import BaseAlert from './BaseAlert.vue'
+import BaseButton from './BaseButton.vue'
+import BaseInput from './BaseInput.vue'
+import BaseCheckbox from './BaseCheckbox.vue'
+import BaseRadio from './BaseRadio.vue'
+import BaseDropdown from './BaseDropdown.vue'
+
+import Card from './Card.vue'
+import CardInfo from './CardInfo.vue'
+import CardStats from './CardStats.vue'
+
 import DocsList from './DocsList.vue'
 import HeadlinesHorizontal from './HeadlinesHorizontal.vue'
 import HeadlinesVertical from './HeadlinesVertical.vue'
 import MarkdownEdit from './MarkdownEdit.vue'
 import MarkdownView from './MarkdownView.vue'
 
+import ScrollTo from './ScrollTo.vue'
+import Splash from './Splash.vue'
+
 export {
+  BaseAlert,
+  BaseButton,
+  BaseInput,
+  BaseCheckbox,
+  BaseRadio,
+  BaseDropdown,
+  Card,
+  CardInfo,
+  CardStats,
   DocsList,
   HeadlinesHorizontal,
   HeadlinesVertical,
   MarkdownEdit,
-  MarkdownView
+  MarkdownView,
+  ScrollTo,
+  Splash
 }
