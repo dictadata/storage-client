367910248f236fdce2f81b60f411e1a484679312
Drew Letcher <drew@drewletcher.net>
2020-03-19 12:30:11 -0500

    makem look pretty

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,50 +1,24 @@
 <template>
   <SlideYUpTransition :duration="animationDuration">
-    <div
-      ref="modal"
-      class="modal fade"
-      @click.self="closeModal"
-      :class="[
+    <div ref="modal" class="modal fade" @click.self="closeModal" :class="[
         { 'show d-block': show },
         { 'd-none': !show },
-        { 'modal-mini': type === 'mini' }
-      ]"
-      v-show="show"
-      tabindex="-1"
-      role="dialog"
-      :aria-hidden="!show"
-    >
-      <div
-        class="modal-dialog"
-        role="document"
-        :class="[
+        { 'modal-mini': type === 'mini' } ]"
+        v-show="show" tabindex="-1" role="dialog" :aria-hidden="!show">
+
+      <div class="modal-dialog" role="document" :class="[
           { 'modal-notice': type === 'notice' },
           { 'modal-dialog-centered': centered },
-          modalClasses
-        ]"
-      >
-        <div
-          class="modal-content"
-          :class="[
+          modalClasses ]">
+
+        <div class="modal-content" :class="[
             gradient ? `bg-gradient-${gradient}` : '',
-            modalContentClasses
-          ]"
-        >
-          <div
-            class="modal-header"
-            :class="[headerClasses]"
-            v-if="$slots.header"
-          >
+            modalContentClasses ]">
+
+          <div class="modal-header" :class="[headerClasses]" v-if="$slots.header">
             <slot name="header"></slot>
             <slot name="close-button">
-              <button
-                type="button"
-                class="close"
-                v-if="showClose"
-                @click="closeModal"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
+              <button type="button" class="close" v-if="showClose" @click="closeModal" data-dismiss="modal" aria-label="Close">
                 <i class="nc2-icon icon-simple-remove"></i>
               </button>
             </slot>
@@ -57,8 +31,11 @@
           <div class="modal-footer" :class="footerClasses" v-if="$slots.footer">
             <slot name="footer"></slot>
           </div>
+
         </div>
+
       </div>
+
     </div>
   </SlideYUpTransition>
 </template>
