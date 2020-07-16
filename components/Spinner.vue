ac7342c12d4af053899767374787bec52b103ebf
Drew Letcher <drew@drewletcher.net>
2020-07-15 23:16:18 -0500

    add spinner

---
+++ components/Spinner.vue
@@ -0,0 +1,29 @@
+<template>
+    <transition name="spinner">
+        <div class="spinner">
+            <div class="spinner-sprite center">
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+                <div class="spinner-blade"></div>
+            </div>
+        </div>
+    </transition>
+</template>
+<script>
+export default {
+  name: 'spinner',
+  components: {},
+  props: {}
+}
+</script>
+<style>
+</style>
