24ccd8c2507943bffe373b973b25231dca24b8e3
drewlab <drew@dictadata.org>
2022-12-18 19:56:41 -0600

    test $user and isGuest

---
+++ test/user/dicta_user.js
@@ -0,0 +1,117 @@
+/**
+ * test/user/dicta_user.js
+ */
+"use strict"
+
+import DictaDataAPI from "../../lib/dictadata-api.js"
+import Account from "../../lib/account.js"
+import $user from '../../lib/user.js'
+import Accounts from "../../lib/accounts.js"
+
+DictaDataAPI.baseURL = "http://dev.dictadata.org:8089"
+/*
+DictaDataAPI.$user.userid = "admin"
+DictaDataAPI.$user.password = "admin"
+DictaDataAPI.$user.roles = [ "User", "Admin" ]
+*/
+
+/**
+ * User
+ */
+async function userTests() {
+  let userid = 'tester'
+  let password = 'testy'
+  let roles = [ "User" ]
+  let user = { userid, password }
+  let account = new Account(user);
+
+  console.log("=== user register")
+  try {
+    account = await $user.register(account)
+    console.log("user: " + JSON.stringify(user))
+    console.log()
+    console.log("account: " + JSON.stringify(account))
+    console.log()
+    console.log("$user: ", JSON.stringify($user))
+    console.log()
+  }
+  catch (err) {
+    console.warn(err.message)
+  }
+
+  console.log("=== user login")
+  try {
+    account = await $user.login(user)
+    $user.password = user.password  // since there is no auth handler in test mode
+    console.log("user: " + JSON.stringify(user))
+    console.log()
+    console.log("account: " + JSON.stringify(account))
+    console.log()
+    console.log("$user: ", JSON.stringify($user))
+    console.log()
+  }
+  catch (err) {
+    console.warn(err.message)
+    return
+  }
+
+  console.log("=== user update")
+  try {
+    $user.profile.displayName = "Tester the Testy"
+    account = await $user.update()
+    console.log("user: " + JSON.stringify(user))
+    console.log()
+    console.log("account: " + JSON.stringify(account))
+    console.log()
+    console.log("$user: ", JSON.stringify($user))
+    console.log()
+  }
+  catch (err) {
+    console.warn(err.message)
+    return
+  }
+
+  console.log("=== user logout")
+  try {
+    let result = await $user.logout()
+    //$user.password = user.password  // since there is no auth handler in test mode
+    console.log("user: " + JSON.stringify(user))
+    console.log()
+    console.log("result: " + JSON.stringify(result))
+    console.log()
+    console.log("$user: ", JSON.stringify($user))
+    console.log()
+  }
+  catch (err) {
+    console.warn(err.message)
+    return
+  }
+
+  console.log("=== user dull")
+  try {
+    await $user.login(new Account({ userid: "admin", password: "admin" }))
+
+    let accounts = new Accounts()
+    let result = await accounts.dull(user)
+    console.log("user: " + JSON.stringify(user))
+    console.log()
+    console.log("result: " + JSON.stringify(result))
+    console.log()
+    console.log("$user: ", JSON.stringify($user))
+    console.log()
+
+    await $user.logout()
+  }
+  catch (err) {
+    console.warn(err.message)
+    return
+  }
+
+}
+
+// test runner
+(async function main() {
+  console.log("dictadata-client tests")
+  await userTests()
+  console.log("done.")
+})()
---
+++ test/user/user_login.js
@@ -0,0 +1,64 @@
+/**
+ * test/user/user_login.js
+ */
+"use strict"
+
+import DictaDataAPI from "../../lib/dictadata-api.js"
+import Account from "../../lib/account.js"
+import $user from '../../lib/user.js'
+
+DictaDataAPI.baseURL = "http://dev.dictadata.org:8089"
+/*
+DictaDataAPI.$user.userid = "admin"
+DictaDataAPI.$user.password = "admin"
+DictaDataAPI.$user.roles = [ "User", "Admin" ]
+*/
+
+async function test_1() {
+  console.log("=== login with account")
+
+  try {
+    let user = { userid: "user", password: "user" }
+
+    let account = await $user.login(user)
+    console.log("account: " + JSON.stringify(account))
+    console.log()
+    console.log("$user: " + JSON.stringify($user))
+    console.log()
+    console.log("isAuthenticated: " + $user.isAuthenticated)
+    console.log("isGuest: " + $user.isGuest)
+  }
+  catch (err) {
+    console.warn("error: " + err.message);
+  }
+}
+
+async function test_2() {
+  console.log("=== login with guest")
+
+  let user = { userid: "dicta", password: "data" }
+
+  $user.login(user, true)
+    .then((account) => {
+      console.log("account: " + JSON.stringify(account))
+      console.log()
+      console.log("$user: " + JSON.stringify($user))
+      console.log()
+      console.log("isAuthenticated: " + $user.isAuthenticated)
+      console.log("isGuest: " + $user.isGuest)
+      })
+    .catch((error) => {
+      console.warn("error: " + error)
+    })
+
+}
+
+(async () => {
+  console.log("=== tests: user login")
+  console.log("$user: " + JSON.stringify($user))
+
+  if (await test_1()) return
+  console.log()
+  if (await test_2()) return
+  console.log("done.")
+})()
