f4bc2bb1edf560642551c8966e90b42028bb8bb8
drewlab <drew@dictadata.org>
2023-01-11 19:24:42 -0600

    replace upload with new Files API

---
+++ test/files/getFile.js
@@ -0,0 +1,32 @@
+/**
+ * test/files/files.js
+ */
+"use strict"
+
+import { login } from "../lib/client.js"
+import Files from "../../lib/files.js"
+
+async function testFiles() {
+
+  try {
+    let files = new Files()
+
+    let url = "https://www.dictadata.org/robots.txt"
+
+    console.log("--- get file")
+    let contents = await files.getFile(url)
+    console.log(contents)
+  }
+  catch (err) {
+    console.warn(err.message)
+    return
+  }
+}
+
+// test runner
+(async () => {
+  console.log("dictadata-client tests")
+  await login()
+  await testFiles()
+  console.log("--- done")
+})()
